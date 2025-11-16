65d2a13aa44492f5184d698046bb9ca4e74af203
Bangash-1122 <ubaidkhan0077800@gmail.com>
2025-11-15 20:37:16 -0500

    feat: enhance healthcheck and like functionalities with error handling and response messages

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,22 +7,101 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const like = await Like.findOne({user: req.user?._id, video: videoId})
+
+    if (like) {
+        await like.remove()
+        return res
+        .status(200)
+        .json(new ApiResponse(200, "Video unliked successfully"))
+    }
+
+    const newLike = new Like({
+        user: req.user?._id,
+        video: videoId
+    })
+
+    await newLike.save()
+    return res
+        .status(201)
+        .json(new ApiResponse(201, "Video liked successfully"))
+     
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
 
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID")
+    }
+
+    const like = await Like.findOne({user: req.user?._id, comment: commentId})
+
+    if (like) {
+        await like.remove()
+        return res
+        .status(200)
+        .json(new ApiResponse(200, "Comment unliked successfully"))
+    }
+
+    const newLike = new Like({
+        user: req.user?._id,
+        comment: commentId
+    })
+
+    await newLike.save()
+    return res
+        .status(201)
+        .json(new ApiResponse(201, "Comment liked successfully"))
+
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet ID")
+    }
+
+    const like = await Like.findOne({user: req.user?._id, tweet: tweetId})
+
+    if (like) {
+        await like.remove()
+        return res
+        .status(200)
+        .json(new ApiResponse(200, "Tweet unliked successfully"))
+    }
+    
+    const newLike = new Like({
+        user: req.user?._id,
+        tweet: tweetId
+    })
+
+    await newLike.save()
+    return res
+        .status(201)
+        .json(new ApiResponse(201, "Tweet liked successfully"))
+
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+
+    const likedVideos = await Like.find({user: req.user?._id, 
+        video: {$ne: null}})
+        .populate('video')
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200,  "Liked videos fetched successfully", likedVideos))
+
 })
 
 export {
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -9,37 +9,196 @@ const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
     //TODO: create playlist
+    if (!name || name.trim() === "") {
+        throw new ApiError(400, "Playlist name is required")
+    }
+
+    const newPlaylist = new Playlist({
+        name: name.trim(),
+        description: description?.trim() || "",
+        user: req.user?._id
+    })
+
+    await newPlaylist.save()
+    return res
+        .status(201)
+        .json(new ApiResponse(201, "Playlist created successfully", newPlaylist))
+
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user ID")
+    }
+
+    const playlists = await Playlist.find({user: userId}).sort({createdAt: -1})
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "User playlists fetched successfully", playlists))
+
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+      
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Playlist fetched successfully", playlist))
+
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+    // TODO: add video to playlist
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+    
+    if (playlist.user.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to add videos to this playlist")
+    }
+
+    const alreadyExists = playlist.videos?.some(video => video.toString() === videoId)
+
+    if (alreadyExists) {
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "Video already in playlist", playlist))
+    }
+
+    playlist.videos.push(videoId)
+    await playlist.save()
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Video added to playlist successfully", playlist))
+
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
 
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video ID")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    if (playlist.user.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to remove videos from this playlist")
+    }
+
+    const originalLength = playlist.videos.length
+
+    playlist.videos = playlist.videos.filter(video => video.toString() !== videoId)
+
+    if (playlist.videos.length === originalLength) {
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "Video not found in playlist", playlist))
+    }
+
+    await playlist.save()
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Video removed from playlist successfully", playlist))
+
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    if (playlist.user.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to delete this playlist")
+    }
+
+    await playlist.deleteOne()
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Playlist deleted successfully"))
+
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    if (playlist.user.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update this playlist")
+    }
+
+    if (name !== undefined) {
+        playlist.name = name.trim()
+    }
+
+    if (description !== undefined) {
+        playlist.description = description.trim()
+    }
+
+    await playlist.save()
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Playlist updated successfully", playlist))
+
 })
 
 export {
